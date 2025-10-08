77fe7641bab68d9688c108d9d4ed470937cba402
fanhousanbu <993921@qq.com>
2025-10-08 14:48:52 +0800

    jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/validator/lib/",
+    "/validator/out/",
+    "/validator/cache/",
+  ],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/main.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+};
